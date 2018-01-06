1650bd7815e5447674900df98b44c1e1a54cb8ed
artman248 <artur.ottlik@gmail.com>
2018-01-06 13:47:44 -0500

    added Dockerfile for  yacoinbuild

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # yacoin-docker
 Dockerfiles for Yacoin
+
+Build the docker image:
+sudo docker build -t yacoinbuild .
+
+Build yacoind:
+sudo docker run -v ABSOLUTEPATH:/out/ yacoinbuild
---
+++ build.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+( cd /yacoin/build && \
+  git clone https://github.com/ya4-old-c-coder/yacoin.git && \
+  cd /yacoin/build/yacoin/src && \
+  ln -s ../../miniupnpc-1.8 miniupnpc && \
+  ln -s makefile.ubuntu makefile && \
+  cd leveldb &&
+  make &&
+  cd .. &&
+  make && \
+  mv yacoind /yacoin/bin && \
+  cd /yacoin/build/yacoin && \
+  qmake yacoin-qt.pro && \
+  make && \
+  mv yacoin-qt /yacoin/bin )
